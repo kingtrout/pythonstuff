acaae69293a9b04bc7e9ffe047eb75fa30974fb0
troutking <markarsenault1313@gmail.com>
2020-12-06 15:35:59 -0500

    Added some more information

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,8 @@
 #name = input("Enter Your Name: ")
 #print("Hey " + name + ", how are you?")
 
+#This is my python calculator
+
 num1 = input("Please enter a number: ")
 num2 = input("Please enter another number: ")
 result = int(num1) + int(num2)
